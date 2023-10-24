897d7e7e01b5b04e4e2d38a75fb9904fce4f878d
Sigmund Freud <124454312+lyricalvr@users.noreply.github.com>
2023-10-24 14:19:11 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,7 +52,7 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'Telegrarm',
+      name: 'Telegram',
       link: 'https://lyrical_vr.t.me',
     },
   ],
